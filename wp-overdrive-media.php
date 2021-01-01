27f3ea9da1172943539f4d8fcf1eb1a3fadc5611
Jeremy Edmiston <jeremy@edmistons.com>
2020-12-31 18:53:08 -0800

    Update wp-overdrive-media.php
    testing icon

--- wp-overdrive-media.php
+++ wp-overdrive-media.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://wp-overdrive.com
  * Author: Edmiston[R+D]
  * Author URI: https://edmistons.com
- * Version: 1.0.11
+ * Version: 1.0.12
  **/
 
 namespace WPOverdrive\Modules;
