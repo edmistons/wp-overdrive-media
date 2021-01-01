b7604d269384b1a19d178e20643bf88dd648d371
Jeremy Edmiston <jeremy@edmistons.com>
2020-12-31 18:59:00 -0800

    Update wp-overdrive-media.php

--- wp-overdrive-media.php
+++ wp-overdrive-media.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://wp-overdrive.com
  * Author: Edmiston[R+D]
  * Author URI: https://edmistons.com
- * Version: 1.0.12
+ * Version: 1.0.13
  **/
 
 namespace WPOverdrive\Modules;
